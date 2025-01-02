d5e8b47174783ea5b4118625a5aacfc4283a3bcd
Rizky Afwan Austin <afwan.rizky96@gmail.com>
2025-01-02 15:26:50 +0700

    add program & latihan section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,60 @@ export default function Login() {
         </div>
       </section>
       {/* end of first big header */}
+
+      {/* start of program latihan section */}
+      <section className="space-y-28 bg-[#bb000e] px-20 py-28 text-white">
+        <h1 className="text-center text-4xl font-bold uppercase">
+          program latihan
+        </h1>
+        <div className="flex flex-row justify-between gap-10">
+          {[...Array(5)].map((_, index) => {
+            return (
+              <div
+                className="flex flex-col items-center gap-10 rounded-3xl bg-black px-2 pb-32 pt-2 text-center"
+                key={index}
+              >
+                <Image
+                  src={"/banner-alt-1.png"}
+                  alt=""
+                  width={"350"}
+                  height={"350"}
+                />
+                <div className="space-y-2">
+                  <p className="text-xl font-bold uppercase text-[#bb000e]">
+                    fun, play & move
+                  </p>
+                  <p className="font-bold">5 - 8 tahun</p>
+                </div>
+                <p className="font-light">
+                  Pada kelompok ini, pemain dikenalkan tentang sepak bola dengan
+                  cara memperbanyak sesi bermain dan menciptakan suasana agar
+                  peserta merasa senang
+                </p>
+                <div className="mt-10 space-y-2 capitalize">
+                  <p className="font-bold underline">program</p>
+                  <div className="font-light">
+                    <p>teknik dasar</p>
+                    <p>circuit game</p>
+                    <p>4v4 game</p>
+                    <p>fair play</p>
+                  </div>
+                </div>
+                <div className="mt-44">
+                  <p className="font-light capitalize">sesi berlatih :</p>
+                  <p className="font-bold capitalize">
+                    minggu 07.00 - 08.30 WIB
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="mx-auto w-fit cursor-pointer rounded-full bg-green-700 px-10 py-5 uppercase text-white transition-colors duration-300 ease-in-out hover:bg-green-800">
+          daftar trial sekarang
+        </div>
+      </section>
+      {/* end of program latihan section */}
     </LandingPageAppshell>
   );
 }
