aaab77e5b82e6f18ac191cd66d2a483590d88072
Rizky Afwan Austin <afwan.rizky96@gmail.com>
2025-02-11 13:25:10 +0700

    change dashboard display if logged in as an athlete

--- src/features/dashboard/view/dashboard-view.tsx
+++ src/features/dashboard/view/dashboard-view.tsx
@@ -1,14 +1,19 @@
 "use client";
 import { BlackBackgroundContainer } from "@/components/container/black-backgorund-container";
 import { DashboardSectionContainer } from "@/components/container/dashboard-section-container";
+import { getAssessmentScoresWithStudentIdQueryOptions } from "@/features/assesmen-pemain/components/actions/get-assessment-scores-with-student-id/query-options";
+import { HasilAsesmenStudentIdTable } from "@/features/assesmen-pemain/components/table/hasil-asesmen-student-id-table";
+import { getBiodataPemainByStudentIdQueryOptions } from "@/features/daftar-pemain/actions/get-biodata-pemain-by-student-id/query-options";
 import { SchoolBanner } from "@/features/school/components/school-banner";
 import { useSchoolInfo } from "@/features/school/hooks/use-school-info";
 import { GetUserByIdResponse } from "@/features/user/actions/get-user-by-id";
 import { isUserAdmin } from "@/features/user/utils/is-user-admin";
 import type { SchoolSession } from "@/lib/session";
+import { calculateAge } from "@/lib/utils/age";
 import { useSchoolStore } from "@/stores/school-store";
-import { Button } from "@mantine/core";
+import { Avatar, Button, Card } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
 import Link from "next/link";
 import { useEffect } from "react";
 import { getSchoolMemberQuantityQueryOptions } from "../actions/get-school-member-quantity/query-options";
@@ -24,11 +29,21 @@ export function DashboardView({
 }: DashboardViewProps) {
   const userIsAdmin = isUserAdmin(userData);
   const { selectedSchool, hydrate, isHydrating } = useSchoolStore();
+  const studentId = selectedSchool?.studentId;
   const { data: schoolInfo } = useSchoolInfo(selectedSchool?.id ?? "");
   // Consider it loading during hydration or when waiting for school info
   const isLoading = isHydrating || (selectedSchool && !schoolInfo);
   const { data: schoolMemberQuantity } = useQuery(
     getSchoolMemberQuantityQueryOptions(schoolInfo?.id ?? ""),
+  );
+  console.log("userData", userData);
+  const { data: biodataData, isLoading: isLoadingBiodata } = useQuery(
+    getBiodataPemainByStudentIdQueryOptions(studentId ?? ""),
+  );
+
+  // Add this query
+  const assessmentScoresQuery = useQuery(
+    getAssessmentScoresWithStudentIdQueryOptions(studentId ?? ""),
   );
 
   useEffect(() => {
@@ -81,66 +96,123 @@ export function DashboardView({
             <p>regional pengguna</p>
           </div>
         </div>
+      ) : schoolInfo &&
+        userData.schools[0].role.toLowerCase().includes("coach") ? ( // For users that are assigned to a school
+        <>
+          <SchoolBanner
+            schoolInfo={schoolInfo}
+            userIsHeadCoach={userIsHeadCoach}
+          />
+
+          <div className="mt-10 grid grid-cols-2 gap-6 md:grid-cols-3 xl:grid-cols-4">
+            <BlackBackgroundContainer>
+              <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
+                {schoolMemberQuantity?.totalPelatih}
+              </p>
+              <p className="relative z-10 font-bold capitalize">
+                total pelatih
+              </p>
+            </BlackBackgroundContainer>
+            <BlackBackgroundContainer>
+              <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
+                {schoolMemberQuantity?.pemainKU8}
+              </p>
+              <p className="relative z-10 font-bold capitalize">
+                pemain kelompok umur 5-8 tahun
+              </p>
+            </BlackBackgroundContainer>
+            <BlackBackgroundContainer>
+              <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
+                {schoolMemberQuantity?.pemainKU10}
+              </p>
+              <p className="relative z-10 font-bold capitalize">
+                pemain kelompok umur 9-12 tahun
+              </p>
+            </BlackBackgroundContainer>
+            <BlackBackgroundContainer>
+              <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
+                {schoolMemberQuantity?.pemainKU13}
+              </p>
+              <p className="relative z-10 font-bold capitalize">
+                pemain kelompok umur 13-15 tahun
+              </p>
+            </BlackBackgroundContainer>
+            <BlackBackgroundContainer>
+              <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
+                {schoolMemberQuantity?.pemainKU15}
+              </p>
+              <p className="relative z-10 font-bold capitalize">
+                pemain kelompok umur 16-18 tahun
+              </p>
+            </BlackBackgroundContainer>
+          </div>
+        </>
       ) : (
-        schoolInfo && ( // For users that are assigned to a school
-          <>
+        <>
+          {schoolInfo && (
             <SchoolBanner
               schoolInfo={schoolInfo}
               userIsHeadCoach={userIsHeadCoach}
             />
-
-            <div className="mt-10 grid grid-cols-2 gap-6 md:grid-cols-3 xl:grid-cols-4">
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.totalPelatih}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  total pelatih
-                </p>
-              </BlackBackgroundContainer>
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.pemainKU8}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  pemain kelompok umur 5-8 tahun
-                </p>
-              </BlackBackgroundContainer>
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.pemainKU10}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  pemain kelompok umur 9-12 tahun
-                </p>
-              </BlackBackgroundContainer>
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.pemainKU13}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  pemain kelompok umur 13-15 tahun
-                </p>
-              </BlackBackgroundContainer>
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.pemainKU15}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  pemain kelompok umur 16-18 tahun
-                </p>
-              </BlackBackgroundContainer>
-              <BlackBackgroundContainer>
-                <p className="relative z-10 text-8xl font-bold max-sm:text-7xl">
-                  {schoolMemberQuantity?.pemainKU17}
-                </p>
-                <p className="relative z-10 font-bold capitalize">
-                  pemain ku-17
-                </p>
-              </BlackBackgroundContainer>
-            </div>
-          </>
-        )
+          )}
+          <div className="mt-4 grid grid-cols-2 items-start gap-4">
+            {isLoadingBiodata ? (
+              <p>Loading biodata...</p>
+            ) : (
+              <Card>
+                <h2 className="mb-4 text-xl font-semibold">Profil Pemain</h2>
+                <div className="flex items-start gap-8 rounded-md border-2 border-gray-200 p-4 shadow-lg">
+                  <Avatar
+                    src={biodataData?.avatarUrl ?? undefined}
+                    size={"20rem"}
+                    radius={"2%"}
+                    alt={`Avatar ${biodataData?.name}`}
+                    name={biodataData?.name ?? undefined}
+                    color="initials"
+                    className="shadow-xl"
+                  />
+                  <div className="flex h-full flex-col justify-between gap-4">
+                    <p className="text-2xl font-semibold capitalize">
+                      Nama: {biodataData?.name}
+                    </p>
+                    <p>
+                      Umur:{" "}
+                      {biodataData?.birthDate
+                        ? calculateAge(new Date(biodataData.birthDate))
+                        : "-"}{" "}
+                      Tahun
+                    </p>
+                    <p>
+                      Tanggal lahir:{" "}
+                      {biodataData?.birthDate
+                        ? dayjs(biodataData.birthDate).format("DD MMMM YYYY")
+                        : "-"}
+                    </p>
+                    <p>
+                      Jenis kelamin:{" "}
+                      {biodataData?.isMale ? "Laki-laki" : "Perempuan"}
+                    </p>
+                    <p>Tinggi Badan: {biodataData?.bodyHeight} cm</p>
+                    <p>Berat Badan: {biodataData?.bodyWeight} kg</p>
+                    <p>Kota: {biodataData?.domisiliKota}</p>
+                    <p>Provinsi: {biodataData?.domisiliProvinsi}</p>
+                  </div>
+                </div>
+              </Card>
+            )}
+            {/* Add HasilAsesmenStudentIdTable component */}
+            <Card>
+              <h2 className="mb-4 text-xl font-semibold">Hasil Asesmen</h2>
+              {assessmentScoresQuery.isLoading ? (
+                <div>Loading assessment data...</div>
+              ) : studentId ? (
+                <HasilAsesmenStudentIdTable studentId={studentId} />
+              ) : (
+                <div>No student ID available</div>
+              )}
+            </Card>
+          </div>
+        </>
       )}
     </DashboardSectionContainer>
   );
